502d827b06ff5781ab3f27acd2e871199d5bb70f
chkhong <hckj1999@gmail.com>
2021-11-10 19:03:28 +0800

    restructure directories

--- app/routers/users.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix='/users')
-
-@router.get('/')
-async def hello():
-  return {"router": "users"}
--- tests/test_users.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-def test_run():
-  response = client.get("/users")
-  assert response.json() == {"router": "users"}
